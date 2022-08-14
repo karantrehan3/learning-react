cea2b45fcc46d1035240d02223231f3e1ae80d45
Karan Trehan <karan.trehan@springworks.in>
2022-08-14 20:35:20 +0530

    Adding Expenses Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", price: "₹10,000", date: new Date(2022, 7, 14) },
+    { title: "UPS", price: "₹2,500", date: new Date(2022, 6, 25) },
+    { title: "Monitor", price: "₹17,000", date: new Date(2021, 10, 4) },
+    { title: "Soundbar", price: "₹6,441", date: new Date(2022, 7, 2) },
+  ];
   return (
     <div>
       <h2>Expense Tracker 101!</h2>
-      <ExpenseItem title="Car Insurance" price="₹10,000" date={new Date()} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }

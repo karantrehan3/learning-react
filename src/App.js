0954180853cde04c6d3f4019155753e5705535ad
Karan Trehan <karan.trehan@springworks.in>
2022-08-14 20:23:40 +0530

    Adding ExpenseDate component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title="Car Insurance"
-        price="₹10,000"
-        date={new Date()}
-      />
+      <h2>Expense Tracker 101!</h2>
+      <ExpenseItem title="Car Insurance" price="₹10,000" date={new Date()} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 /**
@@ -6,16 +7,9 @@ import "./ExpenseItem.css";
  * @returns {html}
  */
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-us", { month: "long" });
-  const year = props.date.getFullYear();
-  const date = props.date.toLocaleString("en-us", { day: "2-digit" });
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{date}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.price}</div>

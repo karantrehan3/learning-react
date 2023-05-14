bc28ae75cb1c325a03b4857d124140f98b66b498
Karan Trehan <karan.trehan@springworks.in>
2023-05-14 22:17:51 +0530

    Added usage of state and event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,5 @@
-import Card from "./Card";
+import React, { useState } from "react";
+import Card from "../Card/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
@@ -8,13 +9,22 @@ import "./ExpenseItem.css";
  * @returns {html}
  */
 function ExpenseItem(props) {
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.price}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
